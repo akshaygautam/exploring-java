3f5247bad9275865f6f6f7c02e181390d38dc410
akshaygautam <akshay.gautam2209@gmail.com>
2020-04-22 12:08:44 +0530

    ObjectVsClassLevelLock

--- src/concurrency/javaCodingProblems/ObjectVsClassLocks.java
+++ src/concurrency/javaCodingProblems/ObjectVsClassLocks.java
@@ -8,26 +8,30 @@ package concurrency.javaCodingProblems;
  */
 public class ObjectVsClassLocks {
 	public static void main(String[] args) {
-		ObjectVsClassLocks obLocks = new ObjectVsClassLocks();
-		obLocks.runThreads();
-		Thread t1 = new Thread(()->{method1(); obLocks.method2(); obLocks.method3();});
-		Thread t2 = new Thread(()->{method1(); obLocks.method2(); obLocks.method3();});
-		 t1.run();
-		 t2.run();
+		ObjectVsClassLocks obClassLocks = new ObjectVsClassLocks();
+		obClassLocks.runThreads();
+		Thread t1 = new Thread(()->{method1(); obClassLocks.method2(); obClassLocks.method3();});
+		Thread t2 = new Thread(()->{method1(); obClassLocks.method2(); obClassLocks.method3();});
+		t1.setName("ClassLockThread1");
+		t2.setName("ClassLockThread2");
+		t1.start();
+		t2.start();
 	}
 	
 	public void runThreads() {
 		ObjectLock oll = new ObjectLock();
 		Thread t1 = new Thread(()->{oll.method1(); oll.method2(); oll.method3();});
 		Thread t2 = new Thread(()->{oll.method1(); oll.method2(); oll.method3();});
-		t1.run();
-		t2.run();
+		t1.setName("ObLockThread1");
+		t2.setName("ObLockThread2");
+		t1.start();
+		t2.start();
 		
 	}
 	
 	class ObjectLock{
 		public synchronized void method1() {
-			System.out.println(this.getClass().getSimpleName()+" -Method1 running ");
+			System.out.println(this.getClass().getSimpleName()+" -Method1 running by: "+ Thread.currentThread().getName());
 			try {
 				Thread.sleep(2000);
 			} catch (InterruptedException e) {
@@ -37,7 +41,7 @@ public class ObjectVsClassLocks {
 		}
 		
 		public void method2() {
-			System.out.println(this.getClass().getSimpleName()+" -Method2 running ");
+			System.out.println(this.getClass().getSimpleName()+" -Method2 running by: "+ Thread.currentThread().getName());
 			synchronized(this) {
 				try {
 					Thread.sleep(2000);
@@ -45,12 +49,12 @@ public class ObjectVsClassLocks {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				System.out.println(this.getClass().getSimpleName()+" -Method2 synchronized block running ");
+				System.out.println(this.getClass().getSimpleName()+" -Method2 synchronized block running by: "+ Thread.currentThread().getName());
 			}
 		}
 		private final Object olLock = new Object();
 		public void method3() {
-			System.out.println(this.getClass().getSimpleName()+" -Method3 running ");
+			System.out.println(this.getClass().getSimpleName()+" -Method3 running by: "+ Thread.currentThread().getName());
 			synchronized (olLock) {
 				try {
 					Thread.sleep(2000);
@@ -58,7 +62,7 @@ public class ObjectVsClassLocks {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				System.out.println(this.getClass().getSimpleName()+" -Method3 synchronized block running ");
+				System.out.println(this.getClass().getSimpleName()+" -Method3 synchronized block running by: "+ Thread.currentThread().getName());
 			}
 		}
 	}
@@ -71,7 +75,7 @@ public class ObjectVsClassLocks {
 	
 	//static method
 	public static synchronized void method1() {
-		System.out.println(ObjectVsClassLocks.class.getSimpleName()+" -Method1 running ");
+		System.out.println(ObjectVsClassLocks.class.getSimpleName()+" -Method1 running by: "+ Thread.currentThread().getName());
 		try {
 			Thread.sleep(2000);
 		} catch (InterruptedException e) {
@@ -82,7 +86,7 @@ public class ObjectVsClassLocks {
 	
 	//synchronized block on .class
 	public void method2() {
-		System.out.println(this.getClass().getSimpleName()+" -Method2 running ");
+		System.out.println(this.getClass().getSimpleName()+" -Method2 running by: "+ Thread.currentThread().getName());
 		synchronized(ObjectVsClassLocks.class) {
 			try {
 				Thread.sleep(2000);
@@ -90,7 +94,7 @@ public class ObjectVsClassLocks {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			System.out.println(this.getClass().getSimpleName()+" -Method2 synchronized block running ");
+			System.out.println(this.getClass().getSimpleName()+" -Method2 synchronized block running by: "+ Thread.currentThread().getName());
 		}
 	}
 	
@@ -98,7 +102,7 @@ public class ObjectVsClassLocks {
 	private final static Object alock = new Object();
 	
 	public void method3() {
-		System.out.println(this.getClass().getSimpleName()+" -Method3 running ");
+		System.out.println(this.getClass().getSimpleName()+" -Method3 running by: "+ Thread.currentThread().getName());
 		synchronized(alock) {
 			try {
 				Thread.sleep(2000);
@@ -106,8 +110,24 @@ public class ObjectVsClassLocks {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			System.out.println(this.getClass().getSimpleName()+" -Method3 synchronized block running ");
+			System.out.println(this.getClass().getSimpleName()+" -Method3 synchronized block running by: "+ Thread.currentThread().getName());
 		}
 	}
 
+	/**
+	 * Some common cases that imply synchronization
+	 * 1. Two threads can execute concurrently a {synchronized static method and a non-static} method of the same class
+	 * This works because the threads acquire locks on different objects. Like in above code method1 is static and method2 and method3
+	 * are non static they are running in parallel.
+	 * 
+	 * 2. Two threads cannot concurrently execute two different {synchronized static methods} of the same class,
+	 * this does not work because the first thread acquires a class level lock.
+	 * 
+	 * 3. Two threads can concurrently execute {non-synchronized, synchronized static and synchronized non-static} 
+	 * 
+	 * 4. It is safe to call a synchronized method from another synchronized method of the same class that requires same lock.
+	 * This works because synchronized is reentrant (as long as the same lock, the lock acquired for first method is used in second method as well)
+	 * 
+	 *  
+	 */
 }
